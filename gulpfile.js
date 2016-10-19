b467acafd80c871e90e79eaad6d347aa8e95d895
Andy Stitt <andystitt829@gmail.com>
2016-10-19 18:49:23 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('sass', function(){
+  return gulp.src('scss/style.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest(''))
+});
+gulp.task('minify-css', function() {
+  return gulp.src('style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('production'));
+});
+
+gulp.task('default', ['sass', 'minify-css']);
